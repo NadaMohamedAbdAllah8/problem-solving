17c23ac01df321afc5e7e883916df6dcc3674521
nada <nada.mohamed@extech.com>
2023-04-06 20:32:09 +0200

    renaming for leetcode

--- Product of Array Except Self.php
+++ Product of Array Except Self.php
@@ -4,29 +4,29 @@ echo 'Product except self sum of [4,5,1,8,2]= [' .
 implode(',', productExceptSelf([4, 5, 1, 8, 2]))
     . ']';
 
-function productExceptSelf($arr)
+function productExceptSelf($nums)
 {
-    $arr_count = count($arr);
+    $nums_count = count($nums);
     $right_arr_calculation = [];
     $left_arr_calculation = [];
     $result_arr = [];
 
     $left_arr_calculation[0] = 1;
-    $right_arr_calculation[$arr_count - 1] = 1;
+    $right_arr_calculation[$nums_count - 1] = 1;
 
     // go forward (left)
-    for ($i = 1; $i < $arr_count; $i++) {
-        $left_arr_calculation[$i] = $arr[$i - 1] * $left_arr_calculation[$i - 1];
+    for ($i = 1; $i < $nums_count; $i++) {
+        $left_arr_calculation[$i] = $nums[$i - 1] * $left_arr_calculation[$i - 1];
     }
     echo 'left calculation=' . implode(',', $left_arr_calculation);
 
     // go backwards (left)
-    for ($i = $arr_count - 2; $i >= 0; $i--) { // $i+1 is count-1
-        $right_arr_calculation[$i] = $right_arr_calculation[$i + 1] * $arr[$i + 1];
+    for ($i = $nums_count - 2; $i >= 0; $i--) { // $i+1 is count-1
+        $right_arr_calculation[$i] = $right_arr_calculation[$i + 1] * $nums[$i + 1];
     }
     echo '<br> right calculation=' . implode(',', $right_arr_calculation) . '<br>';
 
-    for ($i = 0; $i < $arr_count; $i++) {
+    for ($i = 0; $i < $nums_count; $i++) {
         $result_arr[$i] = $right_arr_calculation[$i] * $left_arr_calculation[$i];
     }
     return $result_arr;
