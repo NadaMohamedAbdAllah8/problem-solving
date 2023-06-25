ee156bbf03cec3f7e24cb76b9a5dbf58e016098f
nada <nadamohamedabdallah8@gmail.com>
2023-06-25 21:29:47 +0300

    bottom up fib, calling the functions

--- Fibonacci DP.php
+++ Fibonacci DP.php
@@ -1,9 +1,11 @@
 <?php
+$number=200;
 
+echo fibonacci_using_memoization($number,[null]);
 // memoization
 function fibonacci_using_memoization($number, $memo)
 {
-    if($memo[$number]!=null) {
+    if(isset($memo[$number])&&$memo[$number]!=null) {
         return $memo[$number];
     }
 
@@ -16,4 +18,23 @@ function fibonacci_using_memoization($number, $memo)
     $memo[$number]=$result;
 
     return $result;
+}
+
+echo '<br>';
+echo fibonacci_using_bottom_up($number);
+
+// bottom up
+function fibonacci_using_bottom_up($number)
+{
+    $bottom_up=array();
+    $bottom_up[0]=1;
+    $bottom_up[1]=1;
+    $bottom_up[2]=1;
+
+   for($i=3;$i<=$number;$i++)
+   {
+$bottom_up[$i]=$bottom_up[$i-1]+$bottom_up[$i-2];
+   }
+
+    return $bottom_up[$number];
 }
