8d38ba396c3335c5fd055be7e4a689a1a763fa17
nada <nadamohamedabdallah8@gmail.com>
2023-09-06 12:35:10 +0300

    fix: Check if i==length

--- detect overlapping intervals.php
+++ detect overlapping intervals.php
@@ -1,8 +1,14 @@
 <?php
 
 $intervals = [[1, 3], [5, 7], [2, 4], [6, 8]];
+// echo overLapping($intervals);
 
-echo overLapping($intervals);
+$intervals_2 = [[1, 3], [5, 7], [8,15]];
+
+
+echo '<br>';
+echo overLapping($intervals_2);
+
 function overLapping($intervals): bool
 {
     // sort O(n log(n))
@@ -10,11 +16,16 @@ function overLapping($intervals): bool
         return $a[0] - $b[0];
     });
 
-    //  print_r($intervals);
+//     print_r($intervals);
+//     echo '<br>';
 
     $length = count($intervals);
 
     for ($i = 1; $i++; $i < $length) {
+        if($i == $length) {
+            return false;
+        }
+
         if ($intervals[$i - 1][1] > $intervals[$i][0]) {
             return true;
         }
