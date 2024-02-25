77df7f2bc8b28e58a67490be8909948829c57c3c
nada <nadamohamedabdallah8@gmail.com>
2024-02-25 19:12:59 +0200

    feat: Knapsack example

--- knapsack.php
+++ knapsack.php
@@ -23,14 +23,33 @@ function knapsack($capacity_in_total, $weights, $values, $current_item)
     $previous_max = knapsack($capacity_in_total, $weights, $values, $item_index);
 
     $current_item_and_remaining_weight = $values[$item_index] +
-    knapsack(
-        $capacity_in_total - $weights[$item_index],
-        $weights,
-        $values,
-        $item_index
-    );
+        knapsack(
+            $capacity_in_total - $weights[$item_index],
+            $weights,
+            $values,
+            $item_index
+        );
 
     return max($previous_max, $current_item_and_remaining_weight);
+}
+// ----------------------------------------------------------------
+
+
+// Example usage:
+$capacity = 6;
+$weights = [3, 1, 2, 2, 1];
+$values = [10, 3, 9, 5, 6];
+$num_items = count($weights);
+
+$picked_items = array_fill(0, $num_items, false);
+$max_value = knapsackWithPickedItems($capacity, $weights, $values, $num_items, $picked_items);
+
+echo "<hr>Maximum value: $max_value\n";
+echo "Picked items: ";
+for ($i = 0; $i < $num_items; $i++) {
+    if ($picked_items[$i]) {
+        echo "Item " . ($i + 1) . " ";
+    }
 }
 
 function knapsackWithPickedItems(
@@ -60,7 +79,7 @@ function knapsackWithPickedItems(
     $previous_max = knapsackWithPickedItems($capacity_in_total, $weights, $values, $item_index, $picked_items);
 
     $current_item_and_remaining_weight = $values[$item_index] +
-    knapsackWithPickedItems($capacity_in_total - $weights[$item_index], $weights, $values, $item_index, $picked_items);
+        knapsackWithPickedItems($capacity_in_total - $weights[$item_index], $weights, $values, $item_index, $picked_items);
 
     // Check which option results in a higher value and update the picked_items array accordingly
     if ($previous_max > $current_item_and_remaining_weight) {
@@ -69,22 +88,5 @@ function knapsackWithPickedItems(
     } else {
         $picked_items[$item_index] = true;
         return $current_item_and_remaining_weight;
-    }
-}
-
-// Example usage:
-$capacity = 6;
-$weights = [3, 1, 2, 2, 1];
-$values = [10, 3, 9, 5, 6];
-$num_items = count($weights);
-
-$picked_items = array_fill(0, $num_items, false);
-$max_value = knapsackWithPickedItems($capacity, $weights, $values, $num_items, $picked_items);
-
-echo "<hr>Maximum value: $max_value\n";
-echo "Picked items: ";
-for ($i = 0; $i < $num_items; $i++) {
-    if ($picked_items[$i]) {
-        echo "Item " . ($i + 1) . " ";
     }
 }
